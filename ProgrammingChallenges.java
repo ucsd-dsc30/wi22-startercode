d2f357ad65d60697fdfb4830c93a289e1763969a
scottyang <67733409+ScottY177@users.noreply.github.com>
2022-01-04 15:08:48 -0800

    update starterCode

---
+++ ProgrammingChallenges.java
@@ -0,0 +1,79 @@
+/**
+ * Name: TODO
+ * PID: TODO
+ */
+
+
+/**
+ * TODO
+ * @author TODO
+ * @since TODO
+ */
+public class ProgrammingChallenges {
+
+    ///////// Practice 1 /////////
+
+    public static int[] reverseEvenNumber(int[] arr) {
+        /*TODO*/
+        return null;
+    }
+
+    ///////// Practice 2 /////////
+
+    public static String checkPasswordStrength(String input) {
+        /*TODO*/
+        return null;
+    }
+
+    ///////// Practice 3 /////////
+
+    public static double[] findShortestDistance(double[] x, double[] y){
+        /*TODO*/
+        return null;
+    }
+
+    ///////// Practice 4.1 /////////
+
+    public static String[] getUnique(String[] arr) {
+        /*TODO*/
+        return null;
+    }
+
+    /**
+     * Find the number of unique Element in an array with only unique element
+     * (PROVIDED)
+     * @param arr arr
+     * @return the number of unique element.
+     */
+    public static int getNumOfUniqueElements(String[] arr) {
+        int counter = 0;
+        arr = getUnique(arr);
+        for (String s : arr) {
+            if (s != null) counter++;
+            else break;
+        }
+        return counter;
+    }
+
+    ///////// Practice 4.2 /////////
+
+    public static int[][] oneHotEncode(String[] arr) {
+        /*TODO*/
+        return null;
+    }
+
+    ///////// Practice 5.1 /////////
+
+    public static int[] calculateDerivative(int[] polyArr){
+        /*TODO*/
+        return null;
+    }
+
+    ///////// Practice 5.2 /////////
+
+    public static double getIntersection(int[] poly1, int[] poly2){
+        /*TODO*/
+        return 0;
+    }
+    
+}
