596b890168cab446d0206620b650790dbb3b9f67
SujayTalanki <91350869+SujayTalanki@users.noreply.github.com>
2022-01-12 14:44:41 -0800

    Add files via upload

---
+++ TextEditor.java
@@ -0,0 +1,55 @@
+/*
+    Name: TODO
+    PID:  TODO
+ */
+
+/**
+ * TODO
+ * @author TODO
+ * @since  TODO
+ */
+public class TextEditor {
+
+    /* instance variables */
+    private String text;
+    private IntStack undo;
+    private StringStack deletedText;
+    private IntStack redo;
+    private StringStack insertedText;
+
+    public TextEditor() {
+        /* TODO */
+    }
+
+    public String getText() {
+        /* TODO */
+        return null;
+    }
+
+    public int length() {
+        /* TODO */
+        return 0;
+    }
+
+    public void caseConvert(int i, int j) {
+        /* TODO */
+    }
+
+    public void insert(int i, String input) {
+        /* TODO */
+    }
+
+    public void delete(int i, int j) {
+        /* TODO */
+    }
+
+    public boolean undo() {
+        /* TODO */
+        return false;
+    }
+
+    public boolean redo() {
+        /* TODO */
+        return false;
+    }
+}
